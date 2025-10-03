a9ef956eb19ff37886419fce3cb1bded668b8f21
Aksellercan <akselercan2003@gmail.com>
2025-10-03 18:18:48 +0200

    add: favicon 45x45

--- main.js
+++ main.js
@@ -3,23 +3,25 @@ document.addEventListener("DOMContentLoaded", async function () {
 });
 
 async function getAudibleTabs() {
-    return await browser.tabs.query({audible: true});
+    return await browser.tabs.query({ audible: true });
 }
 
 function GenerateHTMLElements(tab) {
     let media_div = document.createElement("div");
     let media_text = document.createElement("p");
-    media_text.style = "color: blue; font-style: oblique;"
+    let media_ico = document.createElement("img");
+    media_ico.src = tab.favIconUrl;
+    media_ico.style = "style= width: 45px; height: 45px;";
+    media_text.style = "color: blue; font-style: oblique;";
     media_text.textContent = tab.title;
     let switch_tab = document.createElement("button");
     switch_tab.textContent = "switch tab";
     switch_tab.onclick = function () {
         let switch_action = browser.tabs.update(tab.id, { active: true });
-        if (switch_action)
-            console.log(`Switched tab to tabid ${tab.id}`);
-        else
-            console.error("Error switching to tab");
+        if (switch_action) console.debug(`Switched tab to tabid ${tab.id}`);
+        else console.error("Error switching to tab");
     };
+    media_div.appendChild(media_ico);
     media_div.appendChild(media_text);
     media_div.appendChild(switch_tab);
     return media_div;
@@ -29,7 +31,7 @@ async function listTabs(tabs) {
     const media_list = document.getElementById("media-list");
     if (tabs.length < 1) {
         let media = document.createElement("p");
-        media.style = "color: red; font-style: oblique;"
+        media.style = "color: red; font-style: oblique;";
         media.textContent = "No tabs playing audio";
         media_list.appendChild(media);
         console.warn("No tabs playing audio");
@@ -40,8 +42,12 @@ async function listTabs(tabs) {
             let media = GenerateHTMLElements(tab);
             media_list.appendChild(media);
             count++;
-            console.log(`Tab ${count} = ${tab.title}`);
+            console.debug(`Tab ${count} = ${tab.title}`);
         }
     }
-    console.log((count == 0 ? "Found 0 items" : "Found " + count + (count > 1 ? " items": " item")));
+    console.info(
+        count == 0
+            ? "Found 0 items"
+            : "Found " + count + (count > 1 ? " items" : " item")
+    );
 }
